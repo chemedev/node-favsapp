b0309c9d0cb034a276bf0991910eacb26e4ea800
mechell <juanchemell@gmail.com>
2020-03-13 02:28:08 -0300

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-// module.exports = {
-// 	database: {
-// 		host: 'localhost',
-// 		user: 'root',
-// 		password: 'admin',
-// 		database: 'database_links'
-// 	}
-// }
-
 module.exports = {
 	database: {
-		host: 'https://remotemysql.com/phpmyadmin/index.php?db=EckHNp9QqZ',
-		user: 'EckHNp9QqZ',
-		password: 'hrCC9fB5jF',
-		database: 'EckHNp9QqZ'
+		host: 'localhost',
+		user: 'root',
+		password: 'admin',
+		database: 'database_links'
 	}
 }
+
+// module.exports = {
+// 	database: {
+// 		host: 'https://remotemysql.com/phpmyadmin/index.php?db=EckHNp9QqZ',
+// 		user: 'EckHNp9QqZ',
+// 		password: 'hrCC9fB5jF',
+// 		database: 'EckHNp9QqZ'
+// 	}
+// }
