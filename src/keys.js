5533541aa9ec17c30e6e9fcc1a01f650dfe4f834
mechell <juanchemell@gmail.com>
2020-03-13 02:17:59 -0300

    Database from remotemysql

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,17 @@
+// module.exports = {
+// 	database: {
+// 		host: 'localhost',
+// 		user: 'root',
+// 		password: 'admin',
+// 		database: 'database_links'
+// 	}
+// }
+
 module.exports = {
 	database: {
-		host: 'localhost',
-		user: 'root',
-		password: 'admin',
-		database: 'database_links'
+		host: 'https://remotemysql.com/phpmyadmin/index.php?db=EckHNp9QqZ',
+		user: 'EckHNp9QqZ',
+		password: 'hrCC9fB5jF',
+		database: 'EckHNp9QqZ'
 	}
 }
